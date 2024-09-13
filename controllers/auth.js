aa040553fc6bee2c12be0055f3104918bb5ac7f4
Quan <qhoang2373@gmail.com>
2024-09-12 20:10:48 -0700

    create sign in route, page, and session middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,24 +7,38 @@ router.get('/sign-up', (req, res) => {
     res.render('auth/sign-up.ejs')
 })
 
-router.post("/sign-up", async (req, res) => {
-    const userInDatabase = await User.findOne({ username: req.body.username });
-    if (userInDatabase) {
-      return res.send("Username already taken.");
-    }
-    if (req.body.password !== req.body.confirmPassword) {
-        return res.send("Password and Confirm Password must match");
-      }
+router.get("/sign-in", (req, res) => {
+    res.render("auth/sign-in.ejs");
+  });
 
-      const hashedPassword = bcrypt.hashSync(req.body.password, 10);
-        req.body.password = hashedPassword
-        // validation logic
+router.post("/sign-up", async (req, res) => {
+})
+
+router.post("/sign-in", async (req, res) => {
+    const userInDatabase = await User.findOne({ username: req.body.username });
+    if (!userInDatabase) {
+      return res.send("Login failed. Please try again.")
+    }
+    const validPassword = bcrypt.compareSync(
+        req.body.password,
+        userInDatabase.password
+      );
+      if (!validPassword) {
+        return res.send("Login failed. Please try again.");
+      }
+      req.session.user = {
+        username: userInDatabase.username,
+        _id: userInDatabase._id
+      };
+
+      res.redirect("/");
+     });
+          
 
 const user = await User.create(req.body);
 res.send(`Thanks for signing up ${user.username}`);
 
     // res.send("Form submission accepted!");
-  });
   
 
 module.exports = router;
